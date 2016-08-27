2bd912d707a039d62c60e6aa00580e9628946939
m2man <nmduy881994@gmail.com>
2016-08-27 13:51:39 +0700

    Improve Homography and Keydetection
    - Move markers
    - New choice for number of point in homography
    - New approach to keydetection
    - Add a condition for touch fram

--- KeyBoard_Detection/Create_Key_Struct.m
+++ KeyBoard_Detection/Create_Key_Struct.m
@@ -13,29 +13,55 @@ function K = Create_Key_Struct(LayoutPoints, FramePoints, npts, K_Layout)
 LP = LayoutPoints;
 UP = FramePoints;
 
-% adjust Inf Point 1
+% adjust Inf Point 1  - Top Down
 
+% Move B to left-right
 AB = UP(2,:) - UP(1,:);
-ratio = 0.99;
+ratio = 1.01;
 AG = AB * ratio;
 G = AG + UP(1,:);
 UP(2,:) = G;
 
-% adjust Inf Point 2
+% Move A to left-right
+BA = UP(1,:) - UP(2,:);
+ratio = 0.98;
+BG = BA * ratio;
+G = BG + UP(2,:);
+UP(1,:) = G;
 
+% adjust Inf Point 2 - Left Right
+
+% Move A up-down
+DA = UP(1,:) - UP(4,:);
+ratio = 1.02;
+DG = DA * ratio;
+G = DG + UP(4,:);
+UP(1,:) = G;
+
+% Move B up-down
+CB = UP(2,:) - UP(3,:);
+ratio = 1.04;
+CG = CB * ratio;
+G = CG + UP(3,:);
+UP(2,:) = G;
+
+% Move D up-down
 AD = UP(4,:) - UP(1,:);
 ratio = 0.96;
 AE = AD * ratio;
 D = AE + UP(1,:);
 UP(4,:) = D;
 
+% Move C up-down
 BC = UP(3,:) - UP(2,:);
+ratio = 0.96;
 BF = BC * ratio;
 F = BF + UP(2,:);
 UP(3,:) = F;
 oP = LP;nP = UP;
 
-if npts >=8 
+
+if npts >=8
     % find 4 mid point
     P1 = zeros(4,2);
     P2 = zeros(4,2);
@@ -51,23 +77,57 @@ if npts >=8
     P2(4,:) = p2;
     oP = [oP;P1];
     nP = [nP;P2];
-end
-if npts == 12
-    % % mid point of midpoint
-    P1n = zeros(4,2);
-    P2n = zeros(4,2);
-    for i = 1 : 3
-        p1 = (P1(i,:) + P1(i+1,:))/2;
-        p2 = (P2(i,:) + P2(i+1,:))/2;
-        P1n(i,:) = p1;
-        P2n(i,:) = p2;
+    if npts >= 12
+        % % mid point of midpoint
+        P1n = zeros(4,2);
+        P2n = zeros(4,2);
+        for i = 1 : 3
+            p1 = (P1(i,:) + P1(i+1,:))/2;
+            p2 = (P2(i,:) + P2(i+1,:))/2;
+            P1n(i,:) = p1;
+            P2n(i,:) = p2;
+        end
+        p1 = (P1(4,:) + P1(1,:))/2;
+        p2 = (P2(4,:) + P2(1,:))/2;
+        P1n(4,:) = p1;
+        P2n(4,:) = p2;
+        oP = [oP;P1n];
+        nP = [nP;P2n];
+        if npts >= 16
+            % % mid point of midpoint
+            P1 = zeros(4,2);
+            P2 = zeros(4,2);
+            for i = 1 : 3
+                p1 = (P1n(i,:) + P1n(i+1,:))/2;
+                p2 = (P2n(i,:) + P2n(i+1,:))/2;
+                P1(i,:) = p1;
+                P2(i,:) = p2;
+            end
+            p1 = (P1n(4,:) + P1n(1,:))/2;
+            p2 = (P2n(4,:) + P2n(1,:))/2;
+            P1(4,:) = p1;
+            P2(4,:) = p2;
+            oP = [oP;P1];
+            nP = [nP;P2];
+            if npts >= 20
+                % % mid point of midpoint
+                P1n = zeros(4,2);
+                P2n = zeros(4,2);
+                for i = 1 : 3
+                    p1 = (P1(i,:) + P1(i+1,:))/2;
+                    p2 = (P2(i,:) + P2(i+1,:))/2;
+                    P1n(i,:) = p1;
+                    P2n(i,:) = p2;
+                end
+                p1 = (P1(4,:) + P1(1,:))/2;
+                p2 = (P2(4,:) + P2(1,:))/2;
+                P1n(4,:) = p1;
+                P2n(4,:) = p2;
+                oP = [oP;P1n];
+                nP = [nP;P2n];
+            end
+        end
     end
-    p1 = (P1(4,:) + P1(1,:))/2;
-    p2 = (P2(4,:) + P2(1,:))/2;
-    P1n(4,:) = p1;
-    P2n(4,:) = p2;
-    oP = [oP;P1n];
-    nP = [nP;P2n];
 end
 
 H = homography(oP',nP');
--- Key_Detection/keydetection.m
+++ Key_Detection/keydetection.m
@@ -32,7 +32,7 @@ end
 S_avg = S_avg / (64 + 5 - 1);
 
 % TODO: Generalize this para
-thres = 0.25; % threshold for cover ratio. if cover > thres -> still candidate
+thres = 0.1; % threshold for cover ratio. if cover > thres -> still candidate
 
 CK = Candidate_Key(X, K);     % find 4 nearest keys
 
@@ -46,32 +46,52 @@ New_CK = Circle_filter(CK, X, r, K);   % key containing fingertips (posible keys
 
 % New_CK = (CK(vt));
 
-if isempty(New_CK)
-    disp('Nothing here')
-    key = -1;
+if isempty(New_CK) % if no intersection between circle and 4 keys -> nearest of 4
+    %     disp('Nothing here')
+    %     key = -1;
+    disp('Calculate the nearest key of 4');
+    dist = zeros(4,1);
+    for i = 1 : 4
+        dist(i) = norm(X - K{CK(i)}.td(5,:));
+    end
+    [gt,vt] = min(dist);
+    New_CK = (CK(vt));
+    name = Key_name(K{New_CK(1)}.name);
+    disp(['Pressed key is : ' name]);
+    key = name;
 else
     numkey = length(New_CK);
-    if numkey == 1
+    if numkey == 1 % if only 1 -> final
         name = Key_name(K{New_CK(numkey)}.name);
         disp(['Pressed key is : ' name]);
         key = name;
     else
-        disp('Calculate coverage ratio');
-        p = zeros(numkey, 1);
-        for j = 1:numkey
-            p(j) = Cal_cover_ratio(New_CK(j), Skin, K, S_avg);
+        %         disp('Calculate the coverage ratio');
+        %         p = zeros(numkey, 1);
+        %         for j = 1:numkey
+        %             p(j) = Cal_cover_ratio(New_CK(j), Skin, K, S_avg);
+        %         end
+        %         vt = find(p > thres);
+        %         if isempty(vt)
+        %             disp('Nothing for coverage ratio');
+        %             key = -1;
+        %         else
+        %             find key have maxi value of p
+        %             p_max = max(p);
+        %             vt = find(p==p_max);
+        %             name = Key_name(K{New_CK(vt)}.name);
+        %             disp(['Pressed key is : ' name]);
+        %             key = name;
+        %         end
+        disp('Calculate the nearest key of circle'); % if nearest of new candidate keys
+        dist = zeros(numkey,1);
+        for i = 1 : numkey
+            dist(i) = norm(X - K{New_CK(i)}.td(5,:));
         end
-        vt = find(p > thres);
-        if isempty(vt)
-            disp('Nothing for coverage ratio');
-            key = -1;
-        else
-            % find key have maxi value of p
-            p_max = max(p);
-            vt = find(p==p_max);
-            name = Key_name(K{New_CK(vt)}.name);
-            disp(['Pressed key is : ' name]);
-            key = name;
-        end
+        [gt,vt] = min(dist);
+        New_CK = (New_CK(vt));
+        name = Key_name(K{New_CK(1)}.name);
+        disp(['Pressed key is : ' name]);
+        key = name;
     end
 end
